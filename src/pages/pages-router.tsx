88ad1568586019915c42bdf7fbc2e71f346cb4ad
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2024-01-31 14:04:13 -0800

    feat: add header and descriptive text

--- src/pages/pages-router.tsx
+++ src/pages/pages-router.tsx
@@ -1,7 +1,35 @@
+import Link from "next/link";
+import "../globals.css"
+
 export default function Home() {
   console.log('pages router log 1');
   console.log('pages router log 2');
   return (
-    <h1>Pages Router</h1>
+    <div className="p-4">
+      <header className="my-8 flex space-x-4">
+        <Link href="/app-router" className="underline text-blue-500">
+          App Router
+        </Link>
+        <Link href="/pages-router" className="underline text-blue-500">
+          Pages Router
+        </Link>
+      </header>
+      <main className="flex flex-col space-y-8">
+        <h1 className="text-4xl">Pages Router</h1>
+        <p>
+          View the Vercel deployment runtime logs to compare the logging behavior between the app router and the pages router.
+        </p>
+        <p>
+         🔗{' '}
+         <a
+            className="underline text-blue-500"
+            href="https://github.com/iFixit/vercel-app-router-logging-example"
+            target="_blank"
+          >
+            View on GitHub
+          </a>
+        </p>
+      </main>
+    </div>
   );
 }
